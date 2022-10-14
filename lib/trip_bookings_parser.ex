2b742daedaabeb95901afbf3a2d4b75afd6d841c
Sebastian Bucki <sebastian.bucki@gmail.com>
2022-10-15 01:53:56 +0200

    initial commit

---
+++ lib/trip_bookings_parser.ex
@@ -0,0 +1,18 @@
+defmodule TripBookingsParser do
+  @moduledoc """
+  Documentation for `TripBookingsParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TripBookingsParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trip_bookings_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule TripBookingsParserTest do
+  use ExUnit.Case
+  doctest TripBookingsParser
+
+  test "greets the world" do
+    assert TripBookingsParser.hello() == :world
+  end
+end
